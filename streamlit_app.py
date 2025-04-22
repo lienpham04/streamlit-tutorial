a6a6e434d60532de807622205599531c9feaa72d
lienpham04 <122430491+lienpham04@users.noreply.github.com>
2025-04-22 11:16:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title('MLOps app')
+st.title('🚗 Multi-Car Detection with YOLO')
 
 # st.write('Hello world!')
 st.info('👨🏻‍💻 Welcome to the app')
+
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    input_type = st.radio("Select Input Type:", ["Upload Image", "Upload Video", "Live Stream (optional)"])
+    confidence = st.slider("Detection Confidence Threshold", 0.1, 1.0, 0.5)
+
