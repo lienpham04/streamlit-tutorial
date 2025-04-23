f74021f2f6a473e03d0ee61211b39d30adff28c9
lienpham04 <122430491+lienpham04@users.noreply.github.com>
2025-04-23 15:47:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import os
+from PIL import Image
+from serving_pipeline.utils import get_image_array, get_boxes_for_image, draw_boxes_on_image
 
-st.title('🚗 Multi-Car Detection with YOLO')
+st.title('🚗 Multi-label Object Detection with YOLO')
 
 # st.write('Hello world!')
 st.info('👨🏻‍💻 Welcome to the app')
@@ -27,4 +30,19 @@ if uploaded_file:
     results = model(image)
     st.image(results.render()[0], caption="Detection Result", use_column_width=True)
 
+st.set_page_config(page_title="Multi-label Object Detection", layout="centered")
+st.title("🧠 Visualize Labels from CSV")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image_np = get_image_array(uploaded_file)
+    st.image(image_np, caption="Uploaded Image", use_column_width=True)
+
+    if st.button("Draw Bounding Boxes"):
+        image_id = os.path.splitext(uploaded_file.name)[0]
+        h, w = image_np.shape[:2]
+        boxes = get_boxes_for_image(image_id, w, h)
+        image_with_boxes = draw_boxes_on_image(image_np.copy(), boxes)
+        st.image(image_with_boxes, caption="Labeled Bounding Boxes", use_column_width=True)
 
